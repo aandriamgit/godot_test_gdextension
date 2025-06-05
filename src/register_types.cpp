ac56fa0289dc354ba3ce7bd96cfbcde47baf5da4
Aina Rajaonah Andriamanalimanda <aandriam@c1r5s15.42antananarivo.mg>
2025-06-06 00:12:13 +0300

    reloadable true ?

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -1,38 +1,44 @@
+#include "./summator.h"
+/*#include "example_class.h"*/
 #include "register_types.h"
-
 #include <gdextension_interface.h>
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/godot.hpp>
 
-#include "example_class.h"
+using namespace	godot;
 
-using namespace godot;
-
-void initialize_gdextension_types(ModuleInitializationLevel p_level)
+void	initialize_gdextension_types(ModuleInitializationLevel p_level)
 {
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-		return;
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+	{
+		return ;
 	}
-	GDREGISTER_CLASS(ExampleClass);
+	/*GDREGISTER_CLASS(ExampleClass);*/
+	GDREGISTER_CLASS(Summator);
 }
 
-void uninitialize_gdextension_types(ModuleInitializationLevel p_level) {
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-		return;
+void	uninitialize_gdextension_types(ModuleInitializationLevel p_level)
+{
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+	{
+		return ;
 	}
 }
 
 extern "C"
 {
 	// Initialization
-	GDExtensionBool GDE_EXPORT Manaloka00_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
+	GDExtensionBool GDE_EXPORT Manaloka00_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address,
+		GDExtensionClassLibraryPtr p_library,
+		GDExtensionInitialization *r_initialization)
 	{
-		GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+		GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library,
+			r_initialization);
 		init_obj.register_initializer(initialize_gdextension_types);
 		init_obj.register_terminator(uninitialize_gdextension_types);
 		init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
 
-		return init_obj.init();
+		return (init_obj.init());
 	}
 }
--- src/summator.h
+++ src/summator.h
@@ -1,9 +1,23 @@
 #pragma once
 
 #include "godot_cpp/classes/object.hpp"
+#include "godot_cpp/classes/wrapped.hpp"
 
 using namespace	godot;
 
 class Summator : public Object
 {
+	GDCLASS(Summator, Object);
+
+	int count;
+
+  protected:
+	static void _bind_methods();
+
+  public:
+	void add(const int &p_value);
+	void reset();
+	int get_total();
+
+	Summator();
 };
