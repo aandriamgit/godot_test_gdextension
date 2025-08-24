6048ecd7364712cad231e763266eb99742e59e3f
Aina Rajaonah <ainarajaonah230@gmail.com>
2025-08-24 16:25:07 +0300

    wtf

--- src/rtsCamera.cpp
+++ src/rtsCamera.cpp
@@ -1,5 +1,12 @@
 #include "./rtsCamera.hpp"
+#include "godot_cpp/classes/input.hpp"
 #include "godot_cpp/classes/object.hpp"
+#include "godot_cpp/variant/vector2.hpp"
+#include "godot_cpp/variant/vector3.hpp"
+
+void rtsCamera::_bind_methods()
+{
+}
 
 rtsCamera::rtsCamera() : _moveSpeed(0.6)
 {
@@ -15,4 +22,13 @@ void rtsCamera::_ready()
 
 void rtsCamera::_process(float delta)
 {
+	Vector2	inputDir;
+	Vector3	movementDir;
+
+	inputDir = Input::get_singleton()->get_vector("ui_left", "ui_right",
+			"ui_up", "ui_down");
+	movementDir = (get_transform().basis.xform(Vector3(inputDir.x, 0,
+					inputDir.y))).normalized();
+	_moveTarget += _moveSpeed * movementDir;
+	set_position(get_position().lerp(_moveTarget, 0.05));
 }
